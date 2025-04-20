0fa410e10bc2ed4a607024f2d5a09c619e0decd9
Sharon Joseph <90709670+josephsharon07@users.noreply.github.com>
2025-04-20 18:18:34 +0530

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,14 +4,19 @@ const config: CapacitorConfig = {
   appId: 'com.career.guidence',
   appName: 'Empowering Young Minds',
   webDir: 'out',
+
   plugins: {
-    Geolocation: {
-      permissions: {
-        permissions: true
-      }
-    }
-  }
+    SplashScreen: {
+      launchShowDuration: 2000,
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      backgroundColor: '#ffffff',
+      splashFullScreen: true,
+      splashImmersive: true,
+      splashShowOnlyFirstTime: false,
+      splashScreen: 'splash',
+    },
+  },
 };
 
 export default config;
-  
